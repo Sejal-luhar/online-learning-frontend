7da81d14693fca7d53f272c64f0ce9cff420f8db
Sejal-luhar <sejalluhar2001@gmail.com>
2024-12-05 16:29:31 +0530

    url changed

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // Adjust the base URL to your backend
+  baseURL: 'https://online-learning-backend-683d.onrender.com', // Adjust the base URL to your backend
   withCredentials: true,  // Important if you're using sessions or cookies for authentication
 });
 
